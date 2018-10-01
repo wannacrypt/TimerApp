a01c866d1557d25f3ec95d5d974bc4ce41ed9915
bzhandos <baurzhan.zhandos@bam.de>
2018-10-01 11:17:25 +0600

    Additional task

--- app/src/main/java/co/wannacrypt/timerapp/MainActivity.java
+++ app/src/main/java/co/wannacrypt/timerapp/MainActivity.java
@@ -1,14 +1,23 @@
 package co.wannacrypt.timerapp;
 
 import android.content.Intent;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
 
 import entities.City;
 
@@ -20,21 +29,75 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView listView = findViewById(R.id.timerListView);
+        Spinner spinner = findViewById(R.id.mySpinner);
 
         final ArrayList<City> cityList = initCities();
 
-        ArrayAdapter<City> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, cityList);
+        ArrayAdapter<City> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, cityList);
 
-        listView.setAdapter(arrayAdapter);
+        spinner.setAdapter(adapter);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        final Handler handler = new Handler();
+        final Runnable[] runnable = {null};
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(getApplicationContext(), CityActivity.class);
-                City current = cityList.get(position);
-                intent.putExtra(EXTRA_MESSAGE, current.getContinent()  + '/' + current.getCity());
-                startActivity(intent);
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                handler.removeCallbacks(runnable[0]);
+                String location = cityList.get(position).getContinent() + '/' + cityList.get(position).getCity();
+                final Date time = Calendar.getInstance().getTime();
+                final SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss");
+                SimpleDateFormat sdfDay = new SimpleDateFormat("DD");
+                SimpleDateFormat sdfCurrent = new SimpleDateFormat("DD");
+                sdf.setTimeZone(TimeZone.getTimeZone(location));
+                sdfDay.setTimeZone(TimeZone.getTimeZone(location));
+                sdfCurrent.setTimeZone(Calendar.getInstance().getTimeZone());
+
+                String day;
+
+                if (Integer.valueOf(sdfDay.format(time)) > Integer.valueOf(sdfCurrent.format(time))) {
+                    day = "Tomorrow";
+                } else if (Integer.valueOf(sdfDay.format(time)).equals(Integer.valueOf(sdfCurrent.format(time)))) {
+                    day = "Today";
+                } else {
+                    day = "Yesterday";
+                }
+
+                SimpleDateFormat sdfTZ = new SimpleDateFormat("X");
+                sdfTZ.setTimeZone(TimeZone.getTimeZone(location));
+                SimpleDateFormat sdfTZC = new SimpleDateFormat("X");
+                sdfTZC.setTimeZone(Calendar.getInstance().getTimeZone());
+
+                Integer n1 = Integer.valueOf(sdfTZ.format(time));
+                Integer n2 = Integer.valueOf(sdfTZC.format(time));
+                Integer diff = n1 - n2;
+                day += ", ";
+                if (diff > 0)
+                    day += '+';
+                day +=+ diff + "HRS";
+
+                TextView dayTextView = findViewById(R.id.dayTextView);
+                dayTextView.setText(day);
+                TextView cityTextView = findViewById(R.id.cityTextView);
+                cityTextView.setText(location.split("/")[1].replace('_', ' '));
+                final TextView timeTextView = findViewById(R.id.timeTextView);
+                timeTextView.setText(sdf.format(time));
+
+                Runnable run = new Runnable() {
+                    @Override
+                    public void run() {
+                        timeTextView.setText(sdf.format(Calendar.getInstance().getTime()));
+                        handler.postDelayed(this, 1000);
+                    }
+                };
+
+                handler.post(run);
+                runnable[0] = run;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
             }
         });
     }
@@ -44,6 +107,7 @@ public class MainActivity extends AppCompatActivity {
 
         cities.add(new City("Europe", "Amsterdam"));
         cities.add(new City("Europe", "Athens"));
+        cities.add(new City("Asia", "Atyrau"));
         cities.add(new City("Asia", "Beijing"));
         cities.add(new City("Europe", "Berlin"));
         cities.add(new City("America", "Buenos_Aires"));
